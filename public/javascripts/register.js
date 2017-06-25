6a19e9ca69ab1e216c3229c0c7366e5383ef4429
vladi99 <vlpetrovic99@gmail.com>
2017-06-25 12:50:15 +0300

    Fix link and remove unnecessary code in register.js. TODO: import jquery in register.js !!!

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,43 +1,37 @@
-import toastr from 'toastr';
+$("#register-submit").on("click", function () {
+    const username = $("#username").val().trim();
+    const emailAddress = $("#email").val().trim();
+    const password = $("#pw").val().trim();
+    const confirmPassword = $("#pw2").val().trim();
 
-const userController = function (requester) {
-    const userRequester = requester;
+    //TODO: validations
 
-    function register(username, emailAddress, password) {
-        const userData = {
-            username: username,
-            emailAddress: emailAddress,
-            passHash: CryptoJS.SHA1(password).toString(),
-        };
-
-        const promise = new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/register',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(userData),
-                success: response => resolve(response),
-                error: response => reject(response)
-            });
+    register(username, emailAddress, password)
+        .then(function () {
+            toastr.success("You are successfully registered!");
+        }, function () {
+            toastr.error("User with that name already exists");
         });
+});
 
-        return promise;
-    }
+function register(username, emailAddress, password) {
+    const userData = {
+        username: username,
+        emailAddress: emailAddress,
+        passHash: CryptoJS.SHA1(password).toString(),
+    };
 
-    $("#register-submit").on("click", function () {
-        const username = $("#username").val().trim();
-        const emailAddress = $("#email").val().trim();
-        const password = $("#pw").val().trim();
-        const confirmPassword = $("#pw2").val().trim();
-        //TODO: validations
-
-        register(username, emailAddress, password)
-            .then(function () {
-                toastr.success("You are successfully registered!");
-            }, function () {
-                toastr.error("User with that name already exists");
-            });
+    const promise = new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/register',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(userData),
+            success: response => resolve(response),
+            error: response => reject(response)
+        });
     });
 
-};
-export {userController};
+    return promise;
+}
+
