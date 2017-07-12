af1e38840a13a52ef646416a054c8c946348a074
vladi99 <vlpetrovic99@gmail.com>
2017-07-12 19:59:04 +0300

    routers do not give unnecessary data to database and authenticated user when is registered

--- app/routers/users.router.js
+++ app/routers/users.router.js
@@ -32,11 +32,22 @@ const attachTo = (app, data) => {
     });
 
     app.post('/register', (req, res) => {
-        const user = req.body;
-        user.stringProfilePicture = 'defaultpic.jpg';
+        // validate
+        const user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            stringProfilePicture: 'defaultpic.jpg',
+        };
+
         data.users.create(user)
             .then((dbUser) => {
-                return res.redirect('/users/' + dbUser.id);
+                req.login(user, (err) => {
+                    if (err) {
+                        req.flash('error', req);
+                    }
+                    return res.redirect('/users/' + dbUser.id);
+                });
             })
             .catch((err) => {
                 req.flash('error', err);
