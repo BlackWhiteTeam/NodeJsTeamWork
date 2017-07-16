7da1244237a472cb217d879f8b2b6fc1516f571b
ggeneva <g.geneva94@gmail.com>
2017-07-16 19:28:21 +0300

    implement createPost form and logic - needs to be fixed

--- app/routers/posts.router.js
+++ app/routers/posts.router.js
@@ -1,19 +1,39 @@
+const uploadPictureController =
+    require('../controllers/uploadPicture.controller');
+
 const attachTo = (app, data) => {
     app.get('/gallery', (req, res) => {
         return data.posts.getAll()
             .then((posts) => {
-                return res.render('gallery', {
+                return res.render('posts/gallery', {
                     context: posts,
                 });
             });
     });
 
     app.get('/createPost', (req, res) => {
+        return res.render('posts/createPost');
+    });
+
+    app.post('/createPost',
+        uploadPictureController.upload.single('imageupload'), (req, res) => {
+        const photo = req.file;
+        uploadPictureController.uploadPicture(photo);
         const post = {
-            author: req.user,
-            picture: req.body.picture,
-            // Gizi should implement this!
+            author: req.user._id,
+            picture: photo.filename,
+            description: req.body.description,
         };
+        console.log(post);
+
+        data.posts.create(post)
+            .then((dbPost) => {
+                return res.redirect('/users/' + post.author._id);
+            })
+            .catch((err) => {
+                req.flash('error', err);
+                return res.redirect('/');
+            });
     });
 };
 
--- data/base/base.data.js
+++ data/base/base.data.js
@@ -24,6 +24,7 @@ class BaseData {
     }
 
     create(model) {
+        console.log('works');
         if (!this._isModelValid(model)) {
             return Promise.reject('Invalid model');
         }
