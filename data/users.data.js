a7d86d48cf2c67bcec81b767b66c977239abc07b
vladi99 <vlpetrovic99@gmail.com>
2017-07-18 12:06:37 +0300

    add crypting

--- data/users.data.js
+++ data/users.data.js
@@ -1,4 +1,5 @@
 const ObjectId = require('mongodb').ObjectID;
+const CryptoJS = require('crypto-js');
 
 const BaseData = require('./base/base.data');
 const User = require('../models/user.model');
@@ -8,11 +9,26 @@ class UsersData extends BaseData {
         super(db, User, User);
     }
 
+    create(user) {
+        if (!this._isModelValid(user)) {
+            return Promise.reject('Invalid model');
+        }
+
+        // eslint-disable-next-line new-cap
+        user.password = CryptoJS.SHA1(user.password).toString();
+
+        return this.collection.insert(user)
+            .then(() => {
+                return this.ModelClass.toViewModel(user);
+            });
+    }
+
     checkPassword(user, password) {
         if (!user) {
             return Promise.reject('Invalid user');
         }
-        if (user.password !== password) {
+        // eslint-disable-next-line new-cap
+        if (user.password !== CryptoJS.SHA1(password).toString()) {
             return Promise.reject('Invalid password');
         }
         return Promise.resolve(user);
