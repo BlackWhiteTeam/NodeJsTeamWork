c4103527ce5dbbc9d334a7938d677f8db6e70310
ggeneva <g.geneva94@gmail.com>
2017-07-22 01:20:49 +0300

    make likes and dislikes btns + little fixes

--- public/js/about.js
+++ public/js/about.js
@@ -4,7 +4,6 @@ const jumboHeight = $('.jumbotron').outerHeight();
 function parallax() {
     const scrolled = $(window).scrollTop();
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-    $('.bg').css('width', ($(window).width()));
 }
 
 $(window).scroll(function(e) {
