e6479a1c5cea1fb660a7c3182dc2ed294e00ddbb
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-26 17:53:52 +0300

    transfered user router to user controller

--- app/controllers/users.contollers.js
+++ app/controllers/users.contollers.js
@@ -1,4 +1,6 @@
-const userController = (data) => {
+const passport = require('passport');
+
+const userController = (data, helpers) => {
     return {
         renderAllUsers(req, res) {
             return data.users.getAll()
@@ -8,6 +10,105 @@ const userController = (data) => {
                     });
                 });
         },
+        renderRegisterPage(req, res) {
+            return res.render('users/register');
+        },
+        registerUser(req, res) {
+            const user = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                stringProfilePicture: 'defaultpic.png',
+                favorites: [],
+            };
+            // return?
+            data.users.create(user)
+                .then((dbUser) => {
+                    req.login(user, (err) => {
+                        if (err) {
+                            req.flash('error', req);
+                        }
+                        req.flash('success', 'You are registered!');
+                        return res.redirect('/users/' + dbUser._id);
+                    });
+                })
+                .catch((err) => {
+                    req.flash('error', err);
+                    return res.redirect('/register');
+                });
+        },
+        renderLoginPage(req, res) {
+            if (req.user) {
+                return res.redirect('/users/' + req.user._id);
+            }
+            return res.render('users/login');
+        },
+        loginUser(req, res) {
+            passport.authenticate('local', {
+                    failureRedirect: '/login',
+                    failureFlash: true,
+                }),
+                (req, res) => {
+                    data.users.getByObjectName(req.body.username)
+                        .then((user) => {
+                            req.flash('success', 'You are logged in!');
+                            res.redirect('/users/' + user._id);
+                        });
+                }
+        },
+        getProfilePage(req, res) {
+            if (req.user) {
+                const id = getIdByUrl(req.url);
+                data.users.getById(id)
+                    .then((user) => {
+                        data.posts.getPostsByUsername(user.name)
+                            .then((posts) => {
+                                res.render('users/profile', {
+                                    context: user,
+                                    posts: posts,
+                                    currentUserId: req.user._id.toString(),
+                                });
+                            });
+                    });
+            } else {
+                res.redirect('/login');
+            }
+        },
+        updateProfilePicture(req, res) {
+            const id = getIdByUrl(req.url);
+            data.users.getByObjectName(req.user.name)
+                .then((user) => {
+                    const currentUserId = user._id.toString();
+                    if (id === currentUserId) {
+                        const photo = req.file;
+                        helpers.uploadPictureHelper.uploadPicture(photo);
+                        data.users.updateProfilePicture(id, photo);
+
+                        req.flash('info', 'File upload successfully.');
+                    } else {
+                        req.flash('error', 'It is not your profile');
+                    }
+                });
+
+            return res.redirect('/users/' + id);
+        },
+        searchUser(req, res) {
+            const canSeeProfiles = !!(req.user);
+            const input = req.body.searchedUser;
+            data.users.getAllUsersByMatchingString(input)
+                .then((users) => {
+                    return res.render('users/all', {
+                        context: users,
+                        lastInput: input,
+                        canSeeProfiles: canSeeProfiles,
+                    });
+                });
+        },
+        userLogout(req, res) {
+            req.logout();
+            res.redirect('/');
+        }
+
     };
 };
 
--- app/routers/users.router.js
+++ app/routers/users.router.js
@@ -1,4 +1,3 @@
-const passport = require('passport');
 const uploadPictureHelper =
     require('../helpers/uploadPicture.helper');
 // put in helper
@@ -10,126 +9,24 @@ const getIdByUrl = (url) => {
 const attachTo = (app, { usersController }) => {
     app.get('/users', usersController.renderAllUsers);
 
-    app.get('/register', (req, res) => {
-        return res.render('users/register');
-    });
+    app.get('/register', usersController.renderRegisterPage);
 
-    app.post('/register', (req, res) => {
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            stringProfilePicture: 'defaultpic.png',
-            favorites: [],
-        };
 
-        data.users.create(user)
-            .then((dbUser) => {
-                req.login(user, (err) => {
-                    if (err) {
-                        req.flash('error', req);
-                    }
-                    req.flash('success', 'You are registered!');
-                    return res.redirect('/users/' + dbUser._id);
-                });
-            })
-            .catch((err) => {
-                req.flash('error', err);
-                return res.redirect('/register');
-            });
-    });
+    app.post('/register', usersController.registerUser);
 
-    app.get('/login', (req, res) => {
-        if (req.user) {
-            return res.redirect('/users/' + req.user._id);
-        }
-        return res.render('users/login');
-    });
 
-    app.post('/login',
-        passport.authenticate('local', {
-            failureRedirect: '/login',
-            failureFlash: true,
-        }),
-        (req, res) => {
-            data.users.getByObjectName(req.body.username)
-                .then((user) => {
-                    req.flash('success', 'You are logged in!');
-                    res.redirect('/users/' + user._id);
-                });
-        }
-    );
+    app.get('/login', usersController.renderLoginPage);
 
-    app.get('/users/:id', (req, res) => {
-        if (req.user) {
-            const id = getIdByUrl(req.url);
-            data.users.getById(id)
-                .then((user) => {
-                    data.posts.getPostsByUsername(user.name)
-                        .then((posts) => {
-                            res.render('users/profile', {
-                            context: user,
-                            posts: posts,
-                            currentUserId: req.user._id.toString(),
-                            });
-                        });
-                });
-        } else {
-            res.redirect('/login');
-        }
-    });
+    app.post('/login', usersController.loginUser);
+
+    app.get('/users/:id', usersController.getProfilePage);
 
     // eslint-disable-next-line
-    app.post('/users/:id', uploadPictureHelper.upload.single('imageupload'), (req, res) => {
-        const id = getIdByUrl(req.url);
-        data.users.getByObjectName(req.user.name)
-            .then((user) => {
-                const currentUserId = user._id.toString();
-                if (id === currentUserId) {
-                    const photo = req.file;
-                    uploadPictureHelper.uploadPicture(photo);
-                    data.users.updateProfilePicture(id, photo);
+    app.post('/users/:id', uploadPictureHelper.upload.single('imageupload'), usersController.updateProfilePicture);
 
-                    req.flash('info', 'File upload successfully.');
-                } else {
-                    req.flash('error', 'It is not your profile');
-                }
-            });
+    app.post('/users', usersController.searchUser);
 
-        return res.redirect('/users/' + id);
-    });
-
-    app.post('/users', (req, res) => {
-        const canSeeProfiles = !!(req.user);
-        const input = req.body.searchedUser;
-        data.users.getAllUsersByMatchingString(input)
-            .then((users) => {
-                return res.render('users/all', {
-                    context: users,
-                    lastInput: input,
-                    canSeeProfiles: canSeeProfiles,
-                });
-            });
-    });
-
-    app.get('/logout', (req, res) => {
-        req.logout();
-        res.redirect('/');
-    });
-
-    app.get('/addToFavorites/:id', (req, res) => {
-        const idPost = getIdByUrl(req.url);
-        const idUser = (req.user._id);
-        data.users.addToFavorites(idUser, idPost);
-        return res.redirect('/myfavorites');
-    });
-
-    app.get('/deleteFromFavorites/:id', (req, res) => {
-        const idPost = getIdByUrl(req.url);
-        const idUser = (req.user._id);
-        data.users.deleteFromFavorites(idUser, idPost);
-        return res.redirect('/myfavorites');
-    });
+    app.get('/logout', usersController.userLogout);
 };
 
 module.exports = { attachTo };
