3c9f05bdadf46d18f8cc243ef1c29ca3cbd09e67
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-28 13:44:42 +0300

    Added getPostsByUsername.js tests

---
+++ tests/unit/data/base/getByID.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const BaseData = require('../../../../data/base/base.data');
+
+const db = {
+    collection: () => { },
+};
+let items = 1;
+
+let ModelClass = null;
+const validator = null;
+let data = null;
+
+const findOne = (item) => {
+    return Promise.resolve();
+};
+
+describe('GetById', () => {
+    beforeEach(() => {
+        sinon.stub(db, 'collection')
+            .callsFake(() => {
+                return {
+                    findOne,
+                };
+            });
+        ModelClass = class {
+        };
+        data = new BaseData(db, ModelClass, validator);
+    });
+    afterEach(() => {
+        db.collection.restore();
+    });
+    it('findOne should be called', () => {
+        return data.getById()
+            .then(() => {
+                expect(true).to.be.true;
+            },
+            () => {
+                expect(false).to.be.true;
+            });
+    });
+});
---
+++ tests/unit/data/posts.data/getPostsByUsername.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const BaseData = require('../../../../data/posts.data');
+
+const db = {
+    collection: () => { },
+};
+const item = 'testResult';
+
+let ModelClass = null;
+const validator = null;
+let data = null;
+
+const toArray = () => {
+    return Promise.resolve(item);
+};
+
+const find = (object) => {
+    return {
+        toArray,
+    };
+};
+
+describe('getPostsByUsername', () => {
+    beforeEach(() => {
+        sinon.stub(db, 'collection')
+            .callsFake(() => {
+                return {
+                    find,
+                };
+            });
+        ModelClass = class {
+        };
+        data = new BaseData(db, ModelClass, validator);
+    });
+    afterEach(() => {
+        db.collection.restore();
+    });
+    // Arrange
+    it('find should be called with correct string', () => {
+        const expectedResult = item;
+        return data.getPostsByUsername(item)
+            .then((result) => {
+                expect(result).to.equal(expectedResult);
+            });
+    });
+});
