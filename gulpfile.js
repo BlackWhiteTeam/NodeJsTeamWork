5f8bcfcbd07d98b1b9a174bcba2ef55087336b19
vladi99 <vlpetrovic99@gmail.com>
2017-07-28 11:45:10 +0300

    add gulp task for test coverage

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
+
+gulp.task('pre-test', () => {
+    return gulp.src([
+        './app/**/*.js',
+        './data/**/*.js',
+        './models/**/*.js',
+    ])
+        .pipe(istanbul({
+            includeUntested: true,
+        }))
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('tests:unit', ['pre-test'], () => {
+    return gulp.src('./tests/unit/data/*.js')
+        .pipe(mocha())
+        .pipe(istanbul.writeReports());
+});
