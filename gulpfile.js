ba83937a3fb516c99b43446679725f2e89512642
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-28 15:33:39 +0300

    Fixed npm start coverage

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('pre-test', () => {
 });
 
 gulp.task('tests:unit', ['pre-test'], () => {
-    return gulp.src('./tests/unit/data/*.js')
+    return gulp.src('./tests/unit/**/*.js')
         .pipe(mocha())
         .pipe(istanbul.writeReports());
 });
