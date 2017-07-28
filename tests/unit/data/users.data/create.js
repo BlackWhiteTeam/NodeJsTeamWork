748563878267a82c9cb7437af1c802443b24a840
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-28 15:29:38 +0300

    Added users.data create tests

--- tests/unit/data/users.data/create.js
+++ tests/unit/data/users.data/create.js
@@ -1,75 +1,70 @@
-// const { expect } = require('chai');
-// const sinon = require('sinon');
+const { expect } = require('chai');
+const sinon = require('sinon');
 
-// const BaseData = require('../../../../data/users.data');
-// const db = {
-//     collection: () => { },
-// };
-// let items = [];
+const BaseData = require('../../../../data/users.data');
+const db = {
+    collection: () => { },
+};
+let items = [];
 
-// let ModelClass = null;
-// let validator = null;
-// let data = null;
-// const model = null;
+let ModelClass = null;
+let validator = null;
+let data = null;
 
-// const toArray = () => {
-//     return Promise.resolve(items);
-// };
+const insert = (something) => {
+    return Promise.resolve(something);
+};
+describe('create(model)', () => {
+    beforeEach(() => {
+        items = [1, 2, 3, 4];
+        sinon.stub(db, 'collection')
+            .callsFake(() => {
+                return { insert };
+            });
+        ModelClass = class {
+        };
+        ModelClass.toViewModel = (providedModel) => {
+            return providedModel;
+        };
+        validator = {
+            isValid: () => { },
+        };
 
-// const insert = (something) => {
-//     return Promise.resolve(something);
-// };
-// describe('create(model)', () => {
-//     beforeEach(() => {
-//         items = [1, 2, 3, 4];
-//         sinon.stub(db, 'collection')
-//             .callsFake(() => {
-//                 return { insert };
-//             });
-//     });
-//     afterEach(() => {
-//         db.collection.restore();
-//     });
-//     describe('with empty invalid viewModel', () => {
-//         beforeEach(() => {
-//             ModelClass = class {
-//             };
-//             ModelClass.toViewModel = (providedModel) => {
-//                 return providedModel;
-//             };
-//             validator = {
-//                 isValid: () => { },
-//             };
+        // Arrange
+        data = new BaseData(db, ModelClass, validator);
+    });
+    afterEach(() => {
+        db.collection.restore();
+        validator.isValid.restore();
+    });
 
-//             // Arrange
-//             data = new BaseData(db, ModelClass, validator);
-//         });
-//         afterEach(() => {
-//             validator.isValid.restore();
-//         });
-//         it('expect to reject', () => {
-//             sinon.stub(validator, 'isValid')
-//                 .callsFake(() => {
-//                     return false;
-//                 });
-//             return data.create(model)
-//                 .then(() => {
-//                     expect(false).to.be.true;
-//                 },
-//                 () => {
-//                     expect(true).to.be.true;
-//                 });
-//         });
-//         it('expect to add correctly', () => {
-//             items = [1, 2, 3, 4];
-//             sinon.stub(validator, 'isValid')
-//                 .callsFake(() => {
-//                     return true;
-//                 });
-//             return data.create(items)
-//                 .then((result) => {
-//                     expect(result).to.deep.equal(items);
-//                 });
-//         });
-//     });
-// });
+    it('expect to reject when invalid', () => {
+        sinon.stub(validator, 'isValid')
+            .callsFake(() => {
+                return false;
+            });
+        return data.create(items)
+            .then(() => {
+                expect(false).to.be.true;
+            },
+            () => {
+                expect(true).to.be.true;
+            });
+    });
+    // it('expect to add correctly when valid', () => {
+    //     const expectedReuslt = {
+    //         0: 1,
+    //         1: 2,
+    //         2: 3,
+    //         3: 4,
+    //     }
+    //     sinon.stub(validator, 'isValid')
+    //         .callsFake(() => {
+    //             return true;
+    //         });
+    //     return data.create(items)
+    //         .then((result) => {
+    //             expect(result).to.contain(expectedReuslt);
+    //         });
+    // });
+});
