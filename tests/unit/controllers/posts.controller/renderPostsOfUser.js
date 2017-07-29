cdbca84b5ec36389c77aa71a41f195f918310501
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-29 20:39:37 +0300

    Added posts.controller renderPostsOfUser tests

---
+++ tests/unit/controllers/posts.controller/renderPostsOfUser.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+
+const postsController =
+    require('../../../../app/controllers/posts.controller');
+
+describe('getPostById', () => {
+    let data = null;
+    let controller = null;
+    const helpers = {
+    };
+
+    let req = null;
+    let res = null;
+
+    beforeEach(() => {
+        res = require('../req.res').getResponseMock();
+    });
+    it('should return correct post', () => {
+        data = {
+            posts: {
+                getPostsByUsername: (user) => {
+                    return Promise.resolve(user);
+                },
+            },
+        };
+        req = require('../req.res').getRequestMock({
+            user: {
+                name: [1, 2, 3],
+            },
+        });
+        controller = postsController(data, helpers);
+        return controller.renderPostsOfUser(req, res)
+            .then(() => {
+                 expect(res.viewName).to.equal('posts/gallery');
+                 return expect (res.context).to.deep.equal({ context: [3, 2, 1] });
+            });
+    });
+});
