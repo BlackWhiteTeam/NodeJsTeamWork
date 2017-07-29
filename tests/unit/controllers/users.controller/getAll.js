a4023701060f02358b0c95222a22a73f4568905d
freakpazo <cvetozar.kalchev@gmail.com>
2017-07-29 12:33:57 +0300

    Added user.controller getAll tests

---
+++ tests/unit/controllers/users.controller/getAll.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+
+const userController =
+    require('../../../../app/controllers/users.controller');
+
+describe('users controller', () => {
+    let data = null;
+    let controller = null;
+    const helpers = {
+
+    };
+    const users = [1, 2, 3, 4];
+
+    let req = null;
+    let res = null;
+
+    beforeEach(() => {
+        data = {
+            users: {
+                getAll() {
+                    return Promise.resolve(users);
+                },
+            },
+        };
+
+        controller = userController(data, helpers);
+        req = require('../req.res').getRequestMock();
+        res = require('../req.res').getResponseMock();
+    });
+
+    it('expect get all to return users', () => {
+        return controller.renderAllUsers(req, res)
+            .then(() => {
+                expect(res.context).to.be.deep.equal({
+                    context: users,
+                });
+                expect(res.viewName).to.be.equal('users/all');
+            });
+    });
+});
